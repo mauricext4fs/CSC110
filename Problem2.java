50d747698964b18a73772d2429846b96b39de333
Maurice Courtois <mcourtois@Vladbook.local>
2017-07-09 08:53:44 +0200

    - First version with both M and C print... need some polishing doh.

--- Problem2.java
+++ Problem2.java
@@ -1,14 +1,12 @@
 public class Problem2 {
-    private static final String MYINITIAL_FIRST = "M";
+    private static final int WIDTH_OF_TEXT = 30;
     private static final String MYINITIAL_LAST = "C";
-    private static final int WIDTH_OF_TEXT = 20;
-    private static final int HEIGHT_OF_TEXT = 20;
 
     public static void main(String[] args)
     {
         Problem2 problem = new Problem2();
         problem.printFirstRow();
-        for (int i = 2; i <= 3; i++) {
+        for (int i = 2; i <= 15; i++) {
             problem.printRow(i);
         }
     }
@@ -30,24 +28,86 @@ public class Problem2 {
         String c;
         m = template;
         c = template;
-        m = String.format("MMMMM          MMMMM");
-        c = String.format(c, 8).replaceAll("8", MYINITIAL_LAST).replaceAll("0", MYINITIAL_LAST);
+        m = String.format("MMMMM                    MMMMM");
+        c = String.format("        CCCCCCCCCCCCCCCCCCCCCC");
         return new Initial(m, c);
     }
 
-   /**
-    * @deprecated
-    *
-    */
-    public Initial formatFirstRowOld()
+
+    public Initial formatSecondRow()
     {
         String template = "%0" + WIDTH_OF_TEXT + "d";
         String m; 
         String c;
-        m = template;
         c = template;
-        m = String.format(m, 8).replaceAll("8", MYINITIAL_FIRST).replaceAll("0", MYINITIAL_FIRST);
-        c = String.format(c, 8).replaceAll("8", MYINITIAL_LAST).replaceAll("0", MYINITIAL_LAST);
+        m = String.format("MMMMMMM                MMMMMMM");
+        c = String.format("   CCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        return new Initial(m, c);
+    }
+
+    public Initial formatThirdRow()
+    {
+        String template = "%0" + WIDTH_OF_TEXT + "d";
+        String m; 
+        String c;
+        c = template;
+        m = String.format("MMM  MMM              MMM  MMM");
+        c = String.format("CCCCCCCCCCC                   ");
+        return new Initial(m, c);
+    }
+
+    public Initial formatFourthRow()
+    {
+        String template = "%0" + WIDTH_OF_TEXT + "d";
+        String m; 
+        String c;
+        c = template;
+        m = String.format("MMM   MMM            MMM   MMM");
+        c = String.format("CCCC                          ");
+        return new Initial(m, c);
+    }
+
+    public Initial formatSixthRow()
+    {
+        String template = "%0" + WIDTH_OF_TEXT + "d";
+        String m; 
+        String c;
+        c = template;
+        m = String.format("MMM    MMM          MMM    MMM");
+        c = String.format("CCCC                          ");
+        return new Initial(m, c);
+    }
+
+    public Initial formatNinthRow()
+    {
+        String template = "%0" + WIDTH_OF_TEXT + "d";
+        String m; 
+        String c;
+        c = template;
+        m = String.format("MMM     MMM        MMM     MMM");
+        c = String.format("CCCCCCCCCCC                   ");
+        return new Initial(m, c);
+    }
+
+    public Initial formatTenthRow()
+    {
+        String template = "%0" + WIDTH_OF_TEXT + "d";
+        String m; 
+        String c;
+        c = template;
+        m = String.format("MMM       MMM    MMM       MMM");
+        c = String.format("   CCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        return new Initial(m, c);
+    }
+
+    public Initial formatEleventhRow()
+    {
+        String template = "%0" + WIDTH_OF_TEXT + "d";
+        String m; 
+        String c;
+        c = template;
+        m = String.format("MMM         MMMMMM         MMM");
+        c = String.format("        CCCCCCCCCCCCCCCCCCCCCC");
         return new Initial(m, c);
     }
 
@@ -61,41 +121,25 @@ public class Problem2 {
             initial = formatSecondRow(); 
         } else if (numRow == 3) {
             initial = formatThirdRow(); 
+        } else if (numRow == 4 || numRow == 5) {
+            initial = formatFourthRow(); 
+        } else if (numRow == 6 || numRow == 7) {
+            initial = formatSixthRow(); 
+        } else if (numRow == 9) {
+            initial = formatNinthRow(); 
+        } else if (numRow == 10) {
+            initial = formatTenthRow(); 
+        } else if (numRow == 11) {
+            initial = formatEleventhRow(); 
         }
-        System.out.printf("\t%s\t\t%s\n", initial.m, initial.c);
+
+
+
+        if (initial.m != null) {
+            System.out.printf("\t%s\t\t%s\n", initial.m, initial.c);
+        }
     }
 
-    public void printSecondRow()
-    {
-        printRow(2); 
-    }
-
-    public void printThirdRow()
-    {
-        printRow(3); 
-    }
-
-    public Initial formatSecondRow()
-    {
-        String template = "%0" + WIDTH_OF_TEXT + "d";
-        String m; 
-        String c;
-        c = template;
-        m = String.format("MMMMMMM    MMMMMMM");
-        c = String.format(c, 8).replaceAll("8", MYINITIAL_LAST).replaceAll("0", MYINITIAL_LAST);
-        return new Initial(m, c);
-    }
-
-    public Initial formatThirdRow()
-    {
-        String template = "%0" + WIDTH_OF_TEXT + "d";
-        String m; 
-        String c;
-        c = template;
-        m = String.format("MMM  MMM    MMM  MMM");
-        c = String.format(c, 8).replaceAll("8", MYINITIAL_LAST).replaceAll("0", MYINITIAL_LAST);
-        return new Initial(m, c);
-    }
 
     /**
      * Struct Initial
