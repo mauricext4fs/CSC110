e7ad96b8e082f3db8b916de862e4ac258d4490b7
Maurice Courtois <mcourtois@vladbook.home>
2017-07-15 23:56:15 +0200

    - Completed with input validation and proper casting of result

--- Programming Project 2/Problem1/FuelCalculator.java
+++ Programming Project 2/Problem1/FuelCalculator.java
@@ -5,19 +5,21 @@ public class FuelCalculator {
     public FuelCalculator()
     {
         int fuelAmount;
-        int fuelCapacity = 1;
+        int fuelCapacity = 0;
         double fuelPercent;
 
         // Initiate a Scanner Object
         Scanner keyboard = new Scanner(System.in);
         
         // The user must tell us the Capacity in the tank
-        System.out.print("Enter the fuel capacity of the Vehicle fuel tank: ");
-        while (!keyboard.hasNextInt()) {
-            System.out.println("Wrong value... please enter whole number bigger than 0: ");
-            keyboard.nextLine();
+        while (fuelCapacity == 0) {
+            System.out.print("Enter the fuel capacity of the Vehicle fuel tank: ");
+            while (!keyboard.hasNextInt()) {
+                System.out.println("Wrong value... please enter whole number bigger than 0: ");
+                keyboard.nextLine();
+            }
+            fuelCapacity = keyboard.nextInt();
         }
-        fuelCapacity = keyboard.nextInt();
 
         // Now Asking for the current amount
         System.out.print("Now enter the current amount in the fuel tank: ");
@@ -27,7 +29,7 @@ public class FuelCalculator {
          * If the current reading is less than 10 percent of the 
          * tank capcity we must print "red" otherwise "green"
          */
-        fuelPercent = (double)(fuelAmount / fuelCapacity);
+        fuelPercent = (double) fuelAmount / fuelCapacity;
         if (fuelPercent < 0.1) {
             System.out.println("red");
         } else {
